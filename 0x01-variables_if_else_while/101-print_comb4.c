69bf6df34bc7f866447aeb8362ed783f13dfcb1f
KayongoYongo <sknsammie10@gmail.com>
2022-09-10 15:34:39 +0000

    Three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,20 +2,42 @@
 /**
  *main - entry point
  *
- *Description: Returns numbers 0 to 10
+ *Description: Returns numbers 0 to 999 under certin conditions
  *
  *Return: always 0
  */
 
 int main(void)
 {
-	/*a represents numbers*/
-	int a = 0;
+	/*i,j and k represents numbers*/
+	int i;
+	int j;
+	int k;
 
-	while (a < 10)
+	for (i = 0; i < 10; i++)
 	{
-		printf("%d", a);
-		a++;
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				if ((i == j) || (i == k) || (j == k))
+				{
+				continue;
+				}
+					if ((i < j) && (j < k) && (i < k))
+					{
+						putchar((i % 10) + '0');
+						putchar((j % 10) + '0');
+						putchar((k % 10) + '0');
+
+						if ((i != 7) || (j !=8) || (k != 9))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
