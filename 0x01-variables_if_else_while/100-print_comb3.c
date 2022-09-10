914f3e8ea1c0d265d4c80f8f0309f0e9e3d2c15f
KayongoYongo <sknsammie10@gmail.com>
2022-09-10 14:36:28 +0000

    double digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,23 +16,23 @@ int main(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
-		{	
-			if ((i % 10) == (j % 10)) 
+		{
+			if ((i % 10) == (j % 10))
 			{
 			continue;
-			}	
+			}
 				if(i > j)
-				{	
+				{
 				continue;
 				}
 					putchar((i % 10) + '0');
 					putchar((j % 10) + '0');
-				
+
 				if (i != 8 || j != 9)
-				{		
+				{
 					putchar(',');
 					putchar(' ');
-				}	
+				}
 		}
 	}
 	putchar('\n');
