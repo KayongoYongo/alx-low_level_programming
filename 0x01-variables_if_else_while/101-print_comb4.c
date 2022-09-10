2b25cc2c7bae2d18d931e2fb976dd5397080238c
KayongoYongo <sknsammie10@gmail.com>
2022-09-10 02:46:22 +0000

    Three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Description: Returns numbers 0 to 10
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+	/*a represents numbers*/
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
+
