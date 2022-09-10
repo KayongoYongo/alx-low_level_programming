1ad81183a5b69620f0d31a7633d7c512ec7c0d88
KayongoYongo <sknsammie10@gmail.com>
2022-09-10 12:12:22 +0000

    No hanging comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,9 +12,14 @@ int main(void)
 /*a represents numbers*/
 	int a = 0;
 
-	while (a < 10)
+	while (a <= 9)
 	{
-		printf("%d", a);
+		putchar(a % 10 + '0');
+		if (a != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		a++;
 	}
 	putchar('\n');
