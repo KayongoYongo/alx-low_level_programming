13fea2c419376ab0bcb0157e526dacd75442b4b0
KayongoYongo <sknsammie10@gmail.com>
2022-09-14 23:52:44 +0000

    Fibonacci up to 98 n

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point of the program.
+ *
+ *@void: Takes an empty value..
+ *
+ *Description: Prints the first 50 fibnacci numbers.
+ *
+ *Return: always 0(success).
+ */
+
+int main(void)
+{
+	float i = 0;
+	float j = 1;
+	float sum = 0;
+	int k;
+
+	for (k = 0; k < 97; k++)
+	{
+		i = j;
+		j = sum;
+		sum = i + j;
+		printf("%.0f", sum);
+		if (k != 96)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
+
