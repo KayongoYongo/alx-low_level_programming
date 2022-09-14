513c552db1c7495ae1ed604cdb50aa01698d1ef0
KayongoYongo <sknsammie10@gmail.com>
2022-09-14 01:20:58 +0000

    Multplication table of 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,6 @@
 #include "main.h"
-
 /**
- *times_table - times the table function
+ *times_table - prints a multiplication table of 9
  *
  */
 
@@ -13,17 +12,25 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j<= 9; j++)
 		{
 			prod = i * j;
-			_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
-
-			if (j < 9)
-			{
-			_putchar(',');
-			_putchar(' ');
+			
+			if (prod <= 9)
+			{	
+				_putchar(prod % 10 + '0');
 			}
+			else 
+			{	
+				_putchar(prod / 10 + '0');
+				_putchar(prod % 10 + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}	
+				
 		}
 		_putchar('\n');
 	}
