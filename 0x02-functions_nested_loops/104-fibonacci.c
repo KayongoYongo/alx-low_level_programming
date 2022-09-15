3e1de24225e6a39173240893bc5c550f75d5f8ce
KayongoYongo <sknsammie10@gmail.com>
2022-09-15 00:11:51 +0000

    First 98 numbers of fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,18 +12,22 @@
 
 int main(void)
 {
-	float i = 0;
-	float j = 1;
+	float i = 1;
+	float j = i + 1;
 	float sum = 0;
 	int k;
 
-	for (k = 0; k < 99; k++)
+	printf("%.0f, ", i);
+	printf("%.0f, ", j);
+
+	for (k = 0; k < 97; k++)
 	{
-		i = j;
-		j = sum;
 		sum = i + j;
 		printf("%.0f", sum);
-		if (k != 98)
+
+		i = j;
+		j = sum;
+		if (k != 96)
 		{
 			printf(", ");
 		}
