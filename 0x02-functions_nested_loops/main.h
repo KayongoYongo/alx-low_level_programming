fca641ebf61ae99c5a910f96adb79e12e44df953
KayongoYongo <sknsammie10@gmail.com>
2022-09-15 23:57:48 +0000

    Times table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -28,3 +28,5 @@ void times_table(void);
 int add(int, int);
 /*This program prints from n to 98*/
 void print_to_98(int n);
+/*This function prints a multiplication table*/
+void print_times_table(int n);
