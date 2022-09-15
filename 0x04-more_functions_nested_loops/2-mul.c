7a81313640e6dc1df3f880c055eae8541e8618f9
KayongoYongo <sknsammie10@gmail.com>
2022-09-15 15:55:42 +0000

    Multiplication of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *mul-multiplies two numbers
+ *
+ *@a: A vaue to be multiplied.
+ *@b: a value to be multiplied.
+ *
+ *Return: The value is the product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,3 +2,5 @@
 int _isupper(int c);
 /*This program checks wherther a character is a letter or a number*/
 int _isdigit(int c);
+/*This function multiplies two integers*/
+int mul(int a, int b);
