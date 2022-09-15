78726740a23b8860e0a6185ce85bcf60f3c77ce1
KayongoYongo <sknsammie10@gmail.com>
2022-09-15 19:20:33 +0000

    Print squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,7 +11,7 @@ void print_square(int size)
 {
 	int i;
 	int j;
-	
+
 	if (size <= 0)
 	{
 		_putchar('\n');
@@ -21,7 +21,7 @@ void print_square(int size)
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
-			{	
+			{
 				_putchar('#');
 			}
 			_putchar('\n');
