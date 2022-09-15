c7247cd60322e07712f4ecec1d2363ca9d2b344e
KayongoYongo <sknsammie10@gmail.com>
2022-09-15 18:02:31 +0000

    From 1 to 14

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,4 +10,5 @@ void print_numbers(void);
 int _putchar(char c);
 /*This function prints from 0 to 9 except 2 and 4*/
 void print_most_numbers(void);
-
+/*Prints from 0 to 14*/
+void more_numbers(void);
