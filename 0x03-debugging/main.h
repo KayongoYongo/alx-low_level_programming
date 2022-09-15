91998ede4c54d9ad5181a6ee86d5a3bd34e353a7
KayongoYongo <sknsammie10@gmail.com>
2022-09-15 11:14:54 +0000

    Positive or negative

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/*This program returns a positive or a negative integer*/
+void positive_or_negative(int i);
+/*This program writes chaacters*/
+int _putchar(char c);
+/*This program prints letters from a to z in lowercase*/
+void print_alphabet(void);
+/*This program prints the alphabet 10 times*/
+void print_alphabet_x10(void);
+/*Checks for lowercase characters*/
+int _islower(int c);
+/*Chechs for c in uppercase and lowercase. Returns 1 if true and 0 if false*/
+int _isalpha(int c);
+/*A program that prints the sign of a number*/
+int print_sign(int n);
+/*A program that prints the absolute value of an integer*/
+int _abs(int);
+/*A program that prints the last digit of a number*/
+int print_last_digit(int);
+/*This program prints all the minutes in 24 hours*/
+void jack_bauer(void);
+/*This program prints the multiples of 9*/
+void times_table(void);
+/*This program adds the sum of two integers*/
+int add(int, int);
+/*This program prints from n to 98*/
+void print_to_98(int n);
+#endif /* MAIN_H */
