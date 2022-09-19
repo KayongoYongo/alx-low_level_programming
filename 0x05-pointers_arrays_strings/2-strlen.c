4c857a277ebe0b147449e0528ecc0a9e75394fdd
KayongoYongo <sknsammie10@gmail.com>
2022-09-19 22:18:21 +0000

    Length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,10 +8,14 @@
 
 int _strlen(char *s)
 {
-	int i;
+	int i = 0;
+	int count = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{}
-	return (i);
+	while (s[i] != '\0')
+	{
+		i++;
+		count++;
+	}
+	return (count);
 }
 
