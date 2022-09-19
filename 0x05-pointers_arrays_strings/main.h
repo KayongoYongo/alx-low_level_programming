9a66004a0bee3016badc5a71e3ccaf0b7f9e54c2
KayongoYongo <sknsammie10@gmail.com>
2022-09-19 16:29:25 +0000

    String length

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,3 +4,5 @@ int _putchar(char c);
 void reset_to_98(int *n);
 /*This function swaps the valus of two integers*/
 void swap_int(int *a, int *b);
+/*This function returns the length of a string*/
+int _strlen(char *s);
