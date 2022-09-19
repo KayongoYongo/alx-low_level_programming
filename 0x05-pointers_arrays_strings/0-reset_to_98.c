a514691b04d6763638737f73134786c54e2ca64c
KayongoYongo <sknsammie10@gmail.com>
2022-09-19 15:34:46 +0000

    Intro to pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *reset_to_98-This function resetes the pointer to 98
+ *
+ *@n: The value being reset
+ *
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+/*This function prints out characters*/
+int _putchar(char c);
+/*This function takes a pointer as int*/
+void reset_to_98(int *n);
