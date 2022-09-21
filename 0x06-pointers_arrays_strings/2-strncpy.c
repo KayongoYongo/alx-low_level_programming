9e73cc7f8eb0f7ac803366820461997a58b35bfb
KayongoYongo <sknsammie10@gmail.com>
2022-09-21 20:29:54 +0000

    Copy a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,9 +17,13 @@ char *_strncpy(char *dest, char *src, int n)
 
 	n = strlen(src);
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
