eee75bd9b968fabe9a007bcaf3b64f15050e64ec
KayongoYongo <sknsammie10@gmail.com>
2022-09-21 17:36:49 +0000

    Concatenate a string using n

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,4 +2,6 @@
 int _putchar(char c);
 /*A function that concatenates two strings*/
 char *_strcat(char *dest, char *src);
+/*A function that concatenates two strings*/
+char *_strncat(char *dest, char *src, int n);
 
