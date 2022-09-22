9e167ce21bd7f1bcf238b0458a7b8788cc1bd3bb
KayongoYongo <sknsammie10@gmail.com>
2022-09-22 18:04:17 +0000

    Lower to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,22 +3,22 @@
 /**
  *string_toupper- A function that converts lowercase to uppercase
  *
- *@s: The variable to be converted
+ *@str: The variable to be converted
  *
  *Return: The value after it has been converted
  */
 
-char *string_toupper(char *s)
+char *string_toupper(char *str)
 {
 	int i;
 
-	for (i = 0; i != '\0' ; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			s[i] = s[i] - 32;
+			str[i] = str[i] - 32;
 		}
 	}
-	return (s);
+	return (str);
 }
 
