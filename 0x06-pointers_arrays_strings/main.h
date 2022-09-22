aac37b0da2a66445e102c9e454ca0bd7378ac39a
KayongoYongo <sknsammie10@gmail.com>
2022-09-22 19:22:49 +0000

    Rot 13

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -16,3 +16,6 @@ char *string_toupper(char *str);
 char *cap_string(char *s);
 /*A function that encodes a string into 1337*/
 char *leet(char *);
+/*A function that encodes a string using rot13*/
+char *rot13(char *);
+
