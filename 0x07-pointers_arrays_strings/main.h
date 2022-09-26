61291486b014e8a101b5fc954b38b70d2ebf3623
KayongoYongo <sknsammie10@gmail.com>
2022-09-26 17:26:47 +0000

    length of prefix

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,6 +4,8 @@ int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 /*A function that copies memory area*/
 char *_memcpy(char *dest, char *src, unsigned int n);
+/*A function that locates a character in a string*/
+char *_strchr(char *s, char c);
 /*A function that gets the length of a prefix sub string*/
 unsigned int _strspn(char *s, char *accept);
 
