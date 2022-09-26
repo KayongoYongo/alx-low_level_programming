8a07c42f39d39089e80ae2e3ae040b34b0d332c9
KayongoYongo <sknsammie10@gmail.com>
2022-09-26 17:10:30 +0000

    Function that copies memory area

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,4 +2,6 @@
 int _putchar(char c);
 /*A function that fills the memory with a constant byte*/
 char *_memset(char *s, char b, unsigned int n);
+/*A function that copies memory area*/
+char *_memcpy(char *dest, char *src, unsigned int n);
 
