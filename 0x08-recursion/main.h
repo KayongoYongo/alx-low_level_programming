f7633bd8870cee4ab6cecf6c93f15b502934e828
KayongoYongo <sknsammie10@gmail.com>
2022-09-28 17:52:54 +0000

    Factorial of a number

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,5 +6,6 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 /*A function that prints the length of a string*/
 int _strlen_recursion(char *s);
-
+/*A function that prints the factorial of a number*/
+int factorial(int n);
 
