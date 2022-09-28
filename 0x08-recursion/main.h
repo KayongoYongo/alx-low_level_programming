4568662dc4c879acaeac2d24eeacf838a9c639cd
KayongoYongo <sknsammie10@gmail.com>
2022-09-28 21:31:23 +0000

    Find squaer root

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,8 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 /*A function that returns the power of a value*/
 int _pow_recursion(int x, int y);
+/*A function that returns a squre root*/
+int _sqrt_recursion(int n);
 /*A function that helps in finding a prime number*/
 int is_prime_number(int n);
 /*A function that prints out a palindrome*/
