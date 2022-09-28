4f9bd566d4e216e17f8889cffd0a809ea0d4b4d4
KayongoYongo <sknsammie10@gmail.com>
2022-09-28 17:02:43 +0000

    A function which prints a string in reverse

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,4 +2,6 @@
 int _putchar(char c);
 /*The function taht prints out a string*/
 void _puts_recursion(char *s);
+/*A function that prints a string in reverse*/
+void _print_rev_recursion(char *s);
 
