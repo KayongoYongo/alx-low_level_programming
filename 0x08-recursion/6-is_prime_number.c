0e5890bdd443f641ed503aa706f8dc6f310f8988
KayongoYongo <sknsammie10@gmail.com>
2022-09-29 09:47:44 +0000

    Primr numbers using recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,10 +9,17 @@
 int helper(int i, int n)
 {
 	if (n % i == 0 && n != i)
+	{
 		return (0);
-	if (n % i != 0 && i < n)
+	}
+	else if (n % i != 0 && i < n)
+	{
 		return (helper(i + 1, n));
-	return (1);
+	}
+	else
+	{
+		return (1);
+	}
 }
 /**
  * is_prime_number - is prime or not
@@ -24,7 +31,12 @@ int is_prime_number(int n)
 	int i = 2;
 
 	if (n < 2)
+	{
 		return (0);
-	return (helper(i, n));
+	}
+	else
+	{
+		return (helper(i, n));
+	}
 }
 
