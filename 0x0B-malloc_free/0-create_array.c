b97825207a39c47bac2232e918140eac577b63ba
KayongoYongo <sknsammie10@gmail.com>
2022-10-04 02:04:39 +0000

    Array of characters

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ *create_array-This function creates an array
+ *
+ *@size: The size of bytes to reserve 
+ *@c: The array
+ *
+ *Return: The array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	s = malloc(size * sizeof(char));
+
+	if (size <= 0)
+	{
+		return (NULL);	
+	}
+	else if (s == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			s[i] = c;
+		}
+		return (s);
+	}
+}
+
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,5 @@
+/*This function prints characters*/
+int _putchar(char c);
+/*The function that creates an array of chars, and initializes it with a specific char.*/
+char *create_array(unsigned int size, char c);
+
