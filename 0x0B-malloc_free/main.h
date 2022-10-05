34bbe9c343838620dcf9b1a7a7f7fd43e0281e50
KayongoYongo <sknsammie10@gmail.com>
2022-10-05 17:47:36 +0000

    Update header file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,4 +2,10 @@
 int _putchar(char c);
 /*The function that creates an array of chars, and initializes it with a specific char.*/
 char *create_array(unsigned int size, char c);
+/*A function that returns a pointer to a newly allocated space*/
+char *_strdup(char *str);
+/*A function that concatenates two strings*/
+char *str_concat(char *s1, char *s2);
+/*A function that returns a pointer to a 2D array of integers*/
+int **alloc_grid(int width, int height);
 
