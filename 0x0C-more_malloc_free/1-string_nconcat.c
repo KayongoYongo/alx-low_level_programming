ed4b7edd0934014272995a9b9cb9d73ed3febf01
KayongoYongo <sknsammie10@gmail.com>
2022-10-06 13:06:58 +0000

    Concatenate string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,6 +33,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	dest = malloc(sizeof(char) * (l1 + n + 1));
 
+	if (dest == NULL)
+		return (NULL);
+
 	if (s1 != NULL && s2 != NULL)
 	{
 		for (i = 0; i < l1 && s1[i] != '\0'; i++)
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,3 +2,5 @@
 void *malloc_checked(unsigned int b);
 /*A function that concatenates two strings*/
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+/*A function that allocates memory for an array*/
+void *_calloc(unsigned int nmemb, unsigned int size);
