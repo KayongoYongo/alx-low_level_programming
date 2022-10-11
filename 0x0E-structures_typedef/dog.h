62ba0ca23cf511c0f88bc0d8fe6646b331abc15e
KayongoYongo <sknsammie10@gmail.com>
2022-10-11 21:40:49 +0000

    struc typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,13 @@ struct dog
 	float age;
 };
 
+
+/**
+ *dog_t - Typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 /*A function that initializes the variable of type struct dog*/
 void print_dog(struct dog *d);
