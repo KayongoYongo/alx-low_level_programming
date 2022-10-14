f99ce2bd1152096c3c48dbd1363331d3a7a3a14c
KayongoYongo <sknsammie10@gmail.com>
2022-10-14 22:15:47 +0000

    Variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+/*A function that returns the sum of its parameters*/
 int sum_them_all(const unsigned int n, ...);
-
+/*A function that prints numbers*/
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
