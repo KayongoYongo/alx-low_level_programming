861e370c932604dbb0b6b17da8eeec7504f653fa
KayongoYongo <sknsammie10@gmail.com>
2022-10-14 17:15:26 +0000

    Header file

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,30 @@
+#ifndef 3_CALC_H
+#define 3_CALC_H
+
+/*This function finds the sum of the two arguments*/
+int op_add(int a, int b);
+/*This function finds the difference between the two arguments*/
+int op_sub(int a, int b);
+/*This function multiplies the two arguments*/
+int op_mul(int a, int b);
+/*This function finds the divison result*/
+int op_div(int a, int b);
+/*This function finds the remainder between the argument*/
+int op_mod(int a, int b);
+
+int (*get_op_func(char *s))(int, int);
+
+/**
+ *struct op - Struct op
+ *
+ *@op: The operator
+ *@f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
+#endif
+
