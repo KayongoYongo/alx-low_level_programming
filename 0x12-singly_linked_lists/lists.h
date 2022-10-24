e38aed08e4211a95eec4838bfaf5972347fd1d1f
KayongoYongo <sknsammie10@gmail.com>
2022-10-24 19:17:42 +0000

    Node at the beginning

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -16,8 +16,11 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
 /*A function that prints all elements of a list*/
 size_t print_list(const list_t *h);
 /*A function that returns the number of elements in a linked list*/
 size_t list_len(const list_t *h);
+/*A function that returns a node at the beginning of the list*/
+list_t *add_node(list_t **head, const char *str);
 #endif /* LISTS_H */
