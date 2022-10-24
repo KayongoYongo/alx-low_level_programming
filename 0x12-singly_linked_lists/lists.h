1f357966cb2e1950b06d0ef6541d2d81c32f8f8d
KayongoYongo <sknsammie10@gmail.com>
2022-10-24 22:19:49 +0000

    Free list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,4 +25,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 /*A function that returns a node at the bginning of a string*/
 list_t *add_node_end(list_t **head, const char *str);
+/*A function that frees a list*/
+void free_list(list_t *head);
 #endif /* LISTS_H */
