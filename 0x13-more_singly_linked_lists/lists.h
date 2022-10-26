fb8552712e1deca7c86de1ed8ccfbcb7029eed33
KayongoYongo <sknsammie10@gmail.com>
2022-10-26 22:40:43 +0000

    Update list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -24,4 +24,6 @@ size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 /*A function that adds a node at the end of a list*/
 listint_t *add_nodeint_end(listint_t **head, const int n);
+/*A function that drees a list*/
+void free_listint(listint_t *head);
 #endif /* LISTS_H */
