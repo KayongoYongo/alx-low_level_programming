095be8dd81be58e56e28c0f2f7c46d0858bafd55
KayongoYongo <sknsammie10@gmail.com>
2022-10-26 13:07:46 +0000

    Add node at the beginning

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -20,4 +20,6 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 /*A function that return the number of elements in a linked list*/
 size_t listint_len(const listint_t *h);
+/*A function that adds a new node at the beginning of a list*/
+listint_t *add_nodeint(listint_t **head, const int n);
 #endif /* LISTS_H */
