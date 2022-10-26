e95b03581070c1cbd6ff65002df99a3f297ffa12
KayongoYongo <sknsammie10@gmail.com>
2022-10-26 19:41:50 +0000

    Lists update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,12 +8,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 /*A function that prints all elements of a list*/
@@ -22,4 +22,6 @@ size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 /*A function that adds a new node at the beginning of a list*/
 listint_t *add_nodeint(listint_t **head, const int n);
+/*A function that adds a node at the end of a list*/
+listint_t *add_nodeint_end(listint_t **head, const int n);
 #endif /* LISTS_H */
