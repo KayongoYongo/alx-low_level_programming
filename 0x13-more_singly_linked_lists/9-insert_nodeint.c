76134a5c7d037b063a6898f3e5d69a163a62f74f
KayongoYongo <sknsammie10@gmail.com>
2022-10-27 18:42:39 +0000

    add node at n

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *tempC, *tempT, *new;
-	unsigned int count, i = 0;
+	unsigned int count, i = 1;
 
 	if (*head == NULL)
 		return (NULL);
