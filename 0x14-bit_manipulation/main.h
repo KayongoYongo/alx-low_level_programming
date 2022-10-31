c376a0024593ae45581602865fff868e10bc0b31
KayongoYongo <sknsammie10@gmail.com>
2022-10-31 19:26:56 +0000

    Print binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,12 @@
 #ifndef BITS_H
 #define BITS_H
+
+/*A function that prints out characters*/
+int _putchar(char c);
 /*A program that converts from binary to an integer*/
 unsigned int binary_to_uint(const char *b);
+/*A program that prints binary*/
+void print_binary(unsigned long int n);
 /*A program that gets the bit of an index*/
 int get_bit(unsigned long int n, unsigned int index);
 /*A function that sets the value of a bit to 1*/
