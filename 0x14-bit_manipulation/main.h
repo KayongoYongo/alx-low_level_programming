b9b5d9aaa988dddc9901249ac4590882f985f31a
KayongoYongo <sknsammie10@gmail.com>
2022-10-31 19:06:09 +0000

    Get bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,4 +2,6 @@
 #define BITS_H
 /*A program that converts from binary to an integer*/
 unsigned int binary_to_uint(const char *b);
+/*A program that gets the bit of an index*/
+int get_bit(unsigned long int n, unsigned int index);
 #endif
