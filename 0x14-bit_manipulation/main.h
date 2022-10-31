e6f1ebe110a282dcaedb370956fd366942bdb312
KayongoYongo <sknsammie10@gmail.com>
2022-10-31 19:14:20 +0000

    Clear bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,6 +4,8 @@
 unsigned int binary_to_uint(const char *b);
 /*A program that gets the bit of an index*/
 int get_bit(unsigned long int n, unsigned int index);
-/*A function that sets the value of a bit to zero*/
+/*A function that sets the value of a bit to 1*/
 int set_bit(unsigned long int *n, unsigned int index);
+/*A function that sets the value of a bit to 0*/
+int clear_bit(unsigned long int *n, unsigned int index);
 #endif
