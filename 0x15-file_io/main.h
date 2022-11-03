7648eb97a05e155e253705bf2cf9fc3d03d60911
KayongoYongo <sknsammie10@gmail.com>
2022-11-03 00:41:53 +0000

    Create a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,4 +8,6 @@
 #include <unistd.h>
 /*A function that reads a text file and prints it to the standard output*/
 ssize_t read_textfile(const char *filename, size_t letters);
+/*A function that creates a file*/
+int create_file(const char *filename, char *text_content);
 #endif
