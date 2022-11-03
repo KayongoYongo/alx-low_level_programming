e8c3e98141f71097c01b8b80df35da62d309ea1f
KayongoYongo <sknsammie10@gmail.com>
2022-11-03 01:21:24 +0000

    Append to the end of the file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,4 +10,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 /*A function that creates a file*/
 int create_file(const char *filename, char *text_content);
+/*A function that appends text at the ned of a file*/
+int append_text_to_file(const char *filename, char *text_content);
 #endif
