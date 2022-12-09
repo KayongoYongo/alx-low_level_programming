5e06fcc86858e40daaba6fd60a45919d2f27fa89
KayongoYongo <sknsammie10@gmail.com>
2022-12-09 22:13:30 +0000

    get nth node

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -28,4 +28,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 /*A function that frees a list*/
 void free_dlistint(dlistint_t *head);
+/*A function that returns the nth node of a linked list*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 #endif /* LISTS_H */
