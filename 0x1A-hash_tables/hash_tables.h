e6400ce1938f75d8f4c53b30f87856e998d2635f
KayongoYongo <sknsammie10@gmail.com>
2023-01-08 15:06:37 +0000

    Header update

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -13,9 +13,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -28,10 +28,9 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
-} hash_table_t;
+	unsigned long int size;
 
-/*A function that creates a hash table*/
+} hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
+/*Create a new hash table*/
 #endif
