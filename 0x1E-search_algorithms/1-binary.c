94701185df3b49a28fd4e248bbc2dd16c43062cb
KayongoYongo <sknsammie10@gmail.com>
2023-04-13 21:42:37 +0000

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -18,7 +18,7 @@ int binary_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	while (l < r)
+	while (l <= r)
 	{
 		m = (l + r) / 2;
 
@@ -33,7 +33,7 @@ int binary_search(int *array, size_t size, int value)
 		else if (value < array[m])
 			r = m - 1;
 
-		else
+		else if (value > array[m])
 			l = m + 1;
 	}
 	return (-1);
