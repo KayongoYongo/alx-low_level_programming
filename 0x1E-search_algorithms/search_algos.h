aa992775177d7dfdcb36f6e12fbb398f1dc052f6
KayongoYongo <sknsammie10@gmail.com>
2023-04-14 23:50:21 +0000

    exponential

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,4 +13,10 @@ int binary_search(int *array, size_t size, int value);
 
 /*The function carries out a jump search*/
 int jump_search(int *array, size_t size, int value);
+
+/*The function carries out binary search for the exponential search*/
+int exp_binary_search(int *array, size_t l, size_t r, int value);
+
+/*The function carries out exponential search*/
+int exponential_search(int *array, size_t size, int value);
 #endif /* SEARCH_ALGOS_H */
